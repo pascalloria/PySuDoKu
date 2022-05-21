d26f12912d06ab39dcb70bfb91b611173f56b3d9
pascalloria <dev.pascalloria@gmail.com>
2022-05-22 00:20:01 +0200

    Factorisation du code

--- main.py
+++ main.py
@@ -35,171 +35,39 @@ def random_append(list1, list2):
     return x
 
 
+def carre(numbers_rows, grid, x, y):
+    r = -1
+    for a in range(x, x + 3):
+        r += 1
+        for i in range(y, y + 3):
+            numbers_free = generate_number()
+            substract_list_in_list(grid[a][:y], numbers_free)
+            substract_list_in_list(numbers_rows[r], numbers_free)
+            for j in range(0, a):
+                search_remove(grid[j][i], numbers_free)
+            if len(numbers_free) >= 1:
+                grid[a][i] = random_append(numbers_free, numbers_rows[r])
+            else:
+                return True
+
+test_squares = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
 def generate_grid():
     grid = []
-
     for i in range(HEIGHT):
-        # generate row with no duplicates HEIGHT time
+        # génération d'une grille de HEIGHT*WITDH
         grid.append(random.sample(numbers, WIDTH))
-    """========== TOP===================="""
-    # 1 er carré
-    numbers_free = generate_number()
-    for e in grid[0][0:3]:
-        search_remove(e, numbers_free)
-    for j in range(1, 3):
-        for i in range(0, 3):
-            grid[j][i] = random.choice(numbers_free)
-            numbers_free.remove(grid[j][i])
 
-    # 2eme carré
-    numbers_free = generate_number()
-    for e in grid[0][3:6]:
-        search_remove(e, numbers_free)
-    for e in grid[1][:3]:
-        search_remove(e, numbers_free)
-    for i in range(3, 6):
-        grid[1][i] = random.choice(numbers_free)
-        numbers_free.remove(grid[1][i])
-    for e in grid[1][:3]:
-        if e not in numbers_free:
-            if e not in grid[0][3:6]:
-                numbers_free.append(e)
-    for i in range(3, 6):
-        grid[2][i] = random.choice(numbers_free)
-        numbers_free.remove(grid[2][i])
+    numbers_rows = [[], [], []]
+    for square in test_squares:
+        x, y = square
+        if x == 3 or 6 :
+            if y == 0:
+                numbers_rows = [[], [], []]
+        if carre(numbers_rows, grid, x, y) ==True:
+            return
 
-    # 3eme carrée
-    numbers_free = generate_number()
-    for e in grid[0][6:9]:
-        search_remove(e, numbers_free)
-    for e in grid[1][:6]:
-        search_remove(e, numbers_free)
-    for i in range(6, 9):
-        if len(numbers_free) >= 1:
-            grid[1][i] = random.choice(numbers_free)
-            numbers_free.remove(grid[1][i])
-        else:
-            return
-    for e in grid[1][:6]:
-        if e not in numbers_free:
-            if e not in grid[0][6:9]:
-                numbers_free.append(e)
-    for e in grid[2][:6]:
-        search_remove(e, numbers_free)
-    for i in range(6, 9):
-        grid[2][i] = random.choice(numbers_free)
-        numbers_free.remove(grid[2][i])
-    """================== millieu==============="""
-    # first line
-    numbers_rows = [[], [], []]
-    for j in range(0, 9):
-        numbers_free = generate_number()
-        for i in range(0, 3):
-            for elem in numbers_rows:
-                substract_list_in_list(elem, numbers_free)
-            search_remove(grid[i][j], numbers_free)
-            for e in numbers_rows:
-                search_remove(e, numbers_free)
-        if len(numbers_free) >= 1:
-            grid[3][j] = random.choice(numbers_free)
-            if j in range(0, 3):
-                numbers_rows[0].append(grid[3][j])
-            elif j in range(3, 6):
-                numbers_rows[1].append(grid[3][j])
-            else:
-                numbers_rows[2].append(grid[3][j])
-        else:
-            return
-    # LIGNE 2 et 3 du carré 1
-    for a in range(4, 6):
-        for i in range(0, 3):
-            numbers_free = generate_number()
-            substract_list_in_list(numbers_rows[0], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[0])
-            else:
-                return
-        # LIGNE 2 et 3 du carré 2
-        for i in range(3, 6):
-            numbers_free = generate_number()
-            substract_list_in_list(grid[a][:3], numbers_free)
-            substract_list_in_list(numbers_rows[1], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[1])
-            else:
-                return
-        # LIGNE 2 et 3 du carré 3
-        for i in range(6, 9):
-            numbers_free = generate_number()
-            substract_list_in_list(grid[a][:6], numbers_free)
-            substract_list_in_list(numbers_rows[2], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[2])
-            else:
-                return
-    "===========BOT=============="
-    numbers_rows = [[], [], []]
-    for j in range(0, 9):
-        numbers_free = generate_number()
-        for i in range(0, 6):
-            for elem in numbers_rows:
-                substract_list_in_list(elem, numbers_free)
-            search_remove(grid[i][j], numbers_free)
-            for e in numbers_rows:
-                search_remove(e, numbers_free)
-        if len(numbers_free) >= 1:
-            grid[6][j] = random.choice(numbers_free)
-            if j in range(0, 3):
-                numbers_rows[0].append(grid[6][j])
-            elif j in range(3, 6):
-                numbers_rows[1].append(grid[6][j])
-            else:
-                numbers_rows[2].append(grid[6][j])
-        else:
-            return
-    # LIGNE 2 et 3 du carré 1
-    for a in range(7, 9):
-        for i in range(0, 3):
-            numbers_free = generate_number()
-            substract_list_in_list(numbers_rows[0], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[0])
-            else:
-                return
-        # LIGNE 2 et 3 du carré 2
-        for i in range(3, 6):
-            numbers_free = generate_number()
-            substract_list_in_list(grid[a][:3], numbers_free)
-            substract_list_in_list(numbers_rows[1], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[1])
-            else:
-                return
-        # LIGNE 2 et 3 du carré 3
-        for i in range(6, 9):
-            numbers_free = generate_number()
-            substract_list_in_list(grid[a][:6], numbers_free)
-            substract_list_in_list(numbers_rows[2], numbers_free)
-            for j in range(0, a):
-                search_remove(grid[j][i], numbers_free)
-            if len(numbers_free) >= 1:
-                grid[a][i] = random_append(numbers_free, numbers_rows[2])
-            else:
-                return
     for i in range(HEIGHT):
-        # print(grid[i])
         grid_complete.append(grid[i])
-
 
 while not len(grid_complete) > 1:
     generate_grid()
@@ -217,7 +85,8 @@ print("====SOLUTION=========")
 
 for i in range(HEIGHT):
     print(grid_solution[i])
-"""============= TEST DE LA GRILLE GENERE============="""
+
+"""==============TEST DE CONFORMITE DE LA GRILLE GENERE=============="""
 
 
 # pour vérifier faire une somme de chaque ligne / chaque colonne et / chaque carré et le résultat doit etre 45
@@ -262,7 +131,6 @@ for i in range(0, 9):
         collumn_tests.append(False)
 
 # liste contenant la position du coin supérieur gauche de chaque carré
-test_squares = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
 # Test des carrés
 for square in test_squares:
     x, y = square
@@ -284,3 +152,5 @@ if not False in row_tests and collumn_tests and square_tests:
     print("La grille a été généré correctement")
 else:
     print("Erreur : La grille n'a pas été généré correctement")
+
+"""==============FIN DU TEST DE CONFORMITE DE LA GRILLE GENERE=============="""
